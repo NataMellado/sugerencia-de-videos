ed317b375bc335195e5ea834ea260d35f741c34a
Natalia Mellado <nataliamelladocani@gmail.com>
2024-02-22 21:36:49 -0300

    Create classes Reproductor and Multimedia in modules.js

---
+++ assets/js/modules.js
@@ -0,0 +1,57 @@
+// Class Multimedia
+class Multimedia {
+    constructor(url) {
+        this._url = url;
+    }
+
+    // Getter
+    get url() {
+        return this._url;
+    }
+
+    // Method that returns a message
+    setInicio() {
+        return `Este método es para realizar un cambio en la URL del video`;
+    }
+}
+
+// Class Reproductor
+class Reproductor extends Multimedia {
+    constructor(url, id) {
+        super(url);
+        this._id = id;
+    }
+
+    // Getter
+    get id() {
+        return this._id;
+    }
+
+    // Method that call the public function insertVideo
+    playMultimedia() {
+        videoModule.insertVideo(this.url, this._id);
+    }
+
+    // Method that receives and adds the start time to the video
+    setInicio(time) {
+        this.id.setAttribute('src', `${this.url}?start=${time}`);
+    }
+}
+
+// Funtion with module pattern and IIFE to insert video
+const videoModule = (function() {
+
+    // Private function
+    const insertVideo = (url, id) => {
+        const iframe = document.getElementById(id);
+        iframe.setAttribute('src', url);
+    }
+
+    // Public function
+    return {
+        insertVideo
+    }
+})();
+
+// Export the classes and the function
+export { Multimedia, Reproductor, videoModule };
