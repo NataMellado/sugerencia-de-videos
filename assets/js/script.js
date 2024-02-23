fd307475342d46026553c20707b6a8cfdcb351a5
Natalia Mellado <nataliamelladocani@gmail.com>
2024-02-22 21:38:01 -0300

    Create instances and method playMultimedia in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+import { Reproductor, Multimedia, videoModule } from './modules.js';
+
+// Instance of musica, pelicula and serie
+const musica = new Reproductor('https://www.youtube.com/embed/zEqb6xbeuCo', 'musica');
+const pelicula = new Reproductor('https://www.youtube.com/embed/8ugaeA-nMTc', 'peliculas');
+const serie = new Reproductor('https://www.youtube.com/embed/IJ_AZCvCacw', 'series');
+
+// Call the method playMultimedia
+
+musica.playMultimedia();
+pelicula.playMultimedia();
+serie.playMultimedia();
